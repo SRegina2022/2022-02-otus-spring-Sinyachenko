1efed51d35d9649e80bbb56b1c71a0c0bb72be3d
sinyachenko <sinyachenko@i-teco.ru>
2022-05-14 10:11:02 +0300

    Remove homework-02 from homework01

--- homework-02/src/main/java/ru/otus/spring/dao/StudentTestDao.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.dao;
-
-import ru.otus.spring.domain.StudentTest;
-
-public interface StudentTestDao {
-    StudentTest getTest(String testName);
-}
--- homework-02/src/main/java/ru/otus/spring/service/StudentTestService.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.domain.StudentTest;
-
-public interface StudentTestService {
-    StudentTest getByName(String testName);
-}
